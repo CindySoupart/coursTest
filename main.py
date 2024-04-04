760082b80354d293d054fdf27094e2eb79240214
CindySoupart <139112712+CindySoupart@users.noreply.github.com>
2024-04-04 11:49:27 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+fichier = open("index.html","w")
+
+fichier.write("<html><body><h1>Site web générer<h1>")
+
+fichier.write("<p>Ceci est le site du client <b>client1</b><p/>")
+
+
+fichier.write("</body></html>")
+
+fichier.close()
